e43f3ee509ccac412fd8af21091ce1ae0716ebcc
TrebuszeQ <trebuszeq@gmail.com>
2024-02-11 16:51:17 +0100

    FixedCapacityStack Algo Wayne 147

---
+++ src/Structures/FixedCapacityStack.java
@@ -0,0 +1,24 @@
+package Structures;
+
+// N = 0
+// Top = a[N-1]
+public class FixedCapacityStack<T> {
+    
+    void push(T item) {
+
+    }
+
+    T pop() {
+
+    }
+
+    boolean isEmpty() {
+
+    }
+
+    int size() {
+
+    }
+
+
+}
